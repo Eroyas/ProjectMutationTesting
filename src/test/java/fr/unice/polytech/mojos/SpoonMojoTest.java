6f471466d342bd6438997e2497f4902bd520cb6d
Yassine Tijani <tijani.yassine@etu.unice.fr>
2016-02-12 08:59:26 +0100

    creation de la mutation NOP.

--- src/test/java/fr/unice/polytech/mojos/SpoonMojoTest.java
+++ src/test/java/fr/unice/polytech/mojos/SpoonMojoTest.java
@@ -1,13 +1,15 @@
-package com.mycompany.app;
+package fr.unice.polytech.mojos;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class SpoonMojoTest
     extends TestCase
 {
     /**
@@ -15,7 +17,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public SpoonMojoTest(String testName)
     {
         super( testName );
     }
@@ -25,14 +27,15 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( SpoonMojoTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() throws MojoFailureException, MojoExecutionException {
+       SpoonMojo s = new SpoonMojo();
+        s.execute();
+
     }
 }
