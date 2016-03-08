7749d444c082cf5b3d1d157b63f2f102b0ce3eb1
Akuni <sn102953@etu.unice.fr>
2016-03-08 13:36:51 +0100

    ADD IfCond true and false

---
+++ src/main/java/fr/unice/polytech/spoonProcesses/IfCondTrueMutation.java
@@ -0,0 +1,28 @@
+package fr.unice.polytech.spoonProcesses;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtIf;
+import spoon.reflect.declaration.CtElement;
+import spoon.support.reflect.code.CtLiteralImpl;
+
+/**
+ * Created by SARROCHE Nicolas on 07/03/16.
+ */
+public class IfCondTrueMutation extends AbstractProcessor<CtElement> {
+    @Override
+    public boolean isToBeProcessed(CtElement candidate) {
+        return candidate instanceof CtIf;
+    }
+
+
+    @Override
+    public void process(CtElement candidate) {
+        if (!(candidate instanceof CtIf)) {
+            return;
+        }
+
+        CtIf op = (CtIf) candidate;
+        op.getCondition().replace(new CtLiteralImpl<Boolean>().setValue(true));
+    }
+
+}
